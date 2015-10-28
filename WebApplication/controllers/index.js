a8d9668bac495ffacf04a339e95a44dfd21b3bca
Mike Brevard <mikebrevard@gmail.com>
2015-10-28 12:54:00 -0700

    added gitignore added project into subfolder

---
+++ WebApplication/controllers/index.js
@@ -0,0 +1,31 @@
+var express = require('express')
+  , router = express.Router()
+  , path = require('path')
+  , bodyParser = require('body-parser')
+
+
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded())
+
+router.get('/test', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views/index.html'))
+})
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views/login.html'))
+})
+
+router.get('/profile', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views/profile.html'))
+})
+
+router.post('/login', function(req, res) {
+	console.log(req.body.action);
+	if(req.body.action == "login")
+		res.send('Login attempt')	
+	else if(req.body.action == "register")
+		res.send('Register attempt')	
+    console.log(req.body.email);
+})
+
+module.exports = router
