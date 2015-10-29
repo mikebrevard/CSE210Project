12816d43ded8dfc770e95df01cfe6cf98ff3ef72
Mike Brevard <mikebrevard@gmail.com>
2015-10-28 21:55:33 -0700

    Added auth. from Parse Started to work on register / login

--- WebApplication/controllers/index.js
+++ WebApplication/controllers/index.js
@@ -1,31 +1,31 @@
-var express = require('express')
-  , router = express.Router()
-  , path = require('path')
-  , bodyParser = require('body-parser')
-
+var express = require('express'), router = express.Router(), path = require('path'), bodyParser = require('body-parser')
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded())
 
 router.get('/test', function(req, res) {
-    res.sendFile(path.join(__dirname, '../views/index.html'))
+	res.sendFile(path.join(__dirname, '../views/index.html'))
 })
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../views/login.html'))
+	res.sendFile(path.join(__dirname, '../views/login.html'))
 })
 
 router.get('/profile', function(req, res) {
-    res.sendFile(path.join(__dirname, '../views/profile.html'))
+	res.sendFile(path.join(__dirname, '../views/profile.html'))
 })
 
 router.post('/login', function(req, res) {
 	console.log(req.body.action);
-	if(req.body.action == "login")
-		res.send('Login attempt')	
-	else if(req.body.action == "register")
-		res.send('Register attempt')	
-    console.log(req.body.email);
+	if (req.body.action == "login")
+		res.send('Login attempt')
+	else if (req.body.action == "register")
+		res.send('Register attempt')
+	console.log(req.body.email);
+	console.log(req.body.inputPassword);
+	
+	// TODO: add in userRegister(req)
+	// userRegister(req);
 })
 
 module.exports = router
